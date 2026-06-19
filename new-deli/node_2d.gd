0138492643a88f74cbca7538bc73d1f0af19c247
fwu10 <fwu@students.mags.school.nz>
2026-06-19 20:06:12 +1200

    Ingredients Delete if not in Place

--- new-deli/node_2d.gd
+++ new-deli/node_2d.gd
@@ -2,6 +2,7 @@ extends Node2D
 var dragging := false 
 var mouse_offset := Vector2.ZERO
 var mouse_inside = false
+var on_sandwich := false
 func _process(delta: float) -> void:
 	if dragging:
 		global_position = get_global_mouse_position() + mouse_offset
@@ -22,15 +23,21 @@ func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int
 			dragging = true 
 		else:
 			dragging = false
+			_ingredients_moving()
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("sandwich"):
-		print("the ingredients is in sandwich")
+		on_sandwich = true
+
+
+func _ingredients_moving() -> void:
+	if on_sandwich:
+		print("test")
+	else:
+		queue_free()
 
 
 func _on_area_2d_area_exited(area: Area2D) -> void:
-	if $Area2D.get_overlapping_areas().size() == 0:
-		print("WORKING")
-		queue_free()
-		
+	if area.is_in_group("sandwich"):
+		on_sandwich = false
