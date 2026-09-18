f0697f01e5d626e370591a2e16473055a7bbe9ba
fwu10 <fwu@students.mags.school.nz>
2026-09-18 13:41:21 +1200

    Final Main Menu Complete
    - Before redoing Required_Ingredients

--- new-deli/menu.gd
+++ new-deli/menu.gd
@@ -1,7 +1,5 @@
 extends Control
 @onready var button
-@onready var normal_texture: Texture2D
-@onready var hover_texture: Texture2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,3 +9,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_texture_button_pressed() -> void:
+	hide()
+
+
+func _on_texture_button_2_pressed() -> void:
+	get_tree().quit()
